4e3457dade6958a042c37cc2f689f65ef34c9e08
riccardo-5 <rtoso3690@gmail.com>
2024-06-04 18:25:33 +0200

    fixed the corrupted .next folder

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0c4e4406c99b8c2aa2f9a5b3bcd709f4@o4507345952047104.ingest.de.sentry.io/4507369907224656",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
